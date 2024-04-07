45acbe98363ae56e810be0a9e2acfc0333b4806e
JJibbz <vkazancev322@gmail.com>
2024-04-07 18:26:48 +0700

    Добавьте файлы проекта.

---
+++ SocialNetwork/PLL/Views/FriendAddingView.cs
@@ -0,0 +1,47 @@
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.PLL.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.PLL.Views
+{
+    public class FriendAddingView
+    {
+        UserService userService;
+
+        public FriendAddingView(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public void Show(User user)
+        {
+            try
+            {
+                var userAddingFriendData = new UserAddingFriendData();
+
+                Console.WriteLine("Введите адрес электронной почты пользователя которого хотите добавить в друзья: ");
+
+                userAddingFriendData.FriendEmail = Console.ReadLine();
+                userAddingFriendData.UserId = user.Id;
+
+                userService.AddFriend(userAddingFriendData);
+
+                SuccessMessage.Show("Пользователь добавлен в друзья!");
+            }
+            catch (UserNotFoundException)
+            {
+                AlertMessage.Show("Пользователя с указанным почтовым адресом не существует!");
+            }
+            catch (Exception)
+            {
+                AlertMessage.Show("Произошла ошибка при добавлении пользователя в друзья!");
+            }
+        }
+    }
+}
---
+++ SocialNetwork/Program.cs
@@ -0,0 +1,46 @@
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.PLL.Views;
+
+namespace SocialNetwork
+{
+    internal class Program
+    {
+        static MessageService messageService;
+        static UserService userService;
+        public static MainView mainView;
+        public static RegistrationView registrationView;
+        public static AuthenticationView authenticationView;
+        public static UserMenuView userMenuView;
+        public static UserInfoView userInfoView;
+        public static UserDataUpdateView userDataUpdateView;
+        public static MessageSendingView messageSendingView;
+        public static UserIncomingMessageView userIncomingMessageView;
+        public static UserOutcomingMessageView userOutcomingMessageView;
+        public static FriendAddingView friendAddingView;
+        public static UserFriendsView userFriendsView;
+        static void Main(string[] args)
+        {
+            userService = new UserService();
+            messageService = new MessageService();
+
+            mainView = new MainView();
+            registrationView = new RegistrationView(userService);
+            authenticationView = new AuthenticationView(userService);
+            userMenuView = new UserMenuView(userService);
+            userInfoView = new UserInfoView();
+            userDataUpdateView = new UserDataUpdateView(userService);
+            messageSendingView = new MessageSendingView(userService, messageService);
+            userIncomingMessageView = new UserIncomingMessageView();
+            userOutcomingMessageView = new UserOutcomingMessageView();
+            friendAddingView = new FriendAddingView(userService);
+            userFriendsView = new UserFriendsView();
+
+            while (true)
+            {
+                mainView.Show();
+            }
+        }
+    }
+}
